e4567b3025f65fa04ced5a32c7a14d292ce2084f
Sergio <srosduran@gmail.com>
2022-09-09 13:46:40 +0200

    then and wrap methods

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -39,4 +39,54 @@ describe('Our first suite', () => {
         cy.get('[data-cy="imputEmail1"]');
     })
 
+    it('second test', () => {
+        cy.visit('/');
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click();
+
+        cy.get('[data-cy="signInButton"]');
+
+        cy.contains('Sign in');
+
+        cy.contains('[status="warning"]', 'Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click();
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+    });
+
+    it.only('then and wrap methods', () => {
+        cy.visit('/');
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click();
+
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email');
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password');
+
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address');
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password');
+
+        cy.contains('nb-card', 'Using the Grid').then(form => {
+            const emailLabelFirst = form.find('[for="inputEmail1"]').text();
+            const passwordLabelFirst = form.find('[for="inputPassword2"]').text();
+            
+            expect(emailLabelFirst).to.equal('Email');
+            expect(passwordLabelFirst).to.equal('Password');
+
+            cy.wrap(form).find('[for="inputEmail1"]').should('contain', 'Email');
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text();
+                expect(passwordLabelFirst).to.equal(passwordLabelSecond);
+            })
+        });
+
+    });
+
 });
